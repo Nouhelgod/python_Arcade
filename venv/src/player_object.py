36fd8130441439bf342885fceb81ce563e6952f1
Nouhelgod <36785937+Nouhelgod@users.noreply.github.com>
2020-11-13 08:22:30 +0600

    Added 4 direction sprite facing support

--- venv/src/player_object.py
+++ venv/src/player_object.py
@@ -10,22 +10,30 @@ class Player(arcade.Sprite):
     def __init__(self, SCREEN_SCALE):
         super().__init__()
 
-
-        player_sprite = spriter.scale('sprites/player.png', SCREEN_SCALE)
-
         self.textures = []
 
-        texture = arcade.load_texture(spriter.flip_horizontal(player_sprite))
+        # Facing left
+        texture = arcade.load_texture(spriter.flip_horizontal(
+            spriter.scale('sprites/player.png', SCREEN_SCALE)))
+
         self.textures.append(texture)
 
+        # Facing right
         texture = arcade.load_texture(spriter.scale('sprites/player.png', SCREEN_SCALE))
+        self.textures.append(texture)
+
+        # Facing up
+        texture = arcade.load_texture(spriter.scale('sprites/player_back.png', SCREEN_SCALE))
+        self.textures.append(texture)
+
+        # Facing down
+        texture = arcade.load_texture(spriter.scale('sprites/player_front.png', SCREEN_SCALE))
         self.textures.append(texture)
 
         self.texture = texture
         self.speed = 5
 
     def update(self):
-        print(f'{self.center_x}, {self.center_y}')
         self.center_x += self.change_x
         self.center_y += self.change_y
 
@@ -33,4 +41,8 @@ class Player(arcade.Sprite):
             self.texture = self.textures[0]
         elif self.change_x > 0:
             self.texture = self.textures[1]
+        elif self.change_x == self.change_y:
+            self.texture = self.textures[3]
+        elif self.change_y > 0:
+            self.texture = self.textures[2]
 

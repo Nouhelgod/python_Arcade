03707d32e246abb4b97498ab4f4101660d0a6d5c
Nouhelgod <36785937+Nouhelgod@users.noreply.github.com>
2020-11-15 16:28:27 +0600

    Reworked text drawing

--- venv/src/UI.py
+++ venv/src/UI.py
@@ -3,10 +3,11 @@ import arcade
 FONT_H = 10
 FONT_C = arcade.color.BLACK
 MARGIN_L = 10
+LINES = []
 
 class Engine(arcade.Window):
 
-    def set_pos(self, LINE):
+    def get_pos(self, LINE):
         return [MARGIN_L + self.view_left,
                 self.SCREEN_H - FONT_H * LINE - self.debug_margin + self.view_bottom]
 
@@ -16,31 +17,24 @@ class Engine(arcade.Window):
         if self.text_global_frame_counter % 10 == 0:
             self.text_FPS = round(1 / delta_time)
 
+    def draw_text(self, text, LINE):
+        pos = Engine.get_pos(self, LINE)
+        arcade.draw_text(text, pos[0], pos[1], FONT_C, FONT_H)
+
+
     def draw_debug(self):
         if self.debug_shown:
-            LINE = 1
-            arcade.draw_text(f'Local frame: {self.text_local_frame_counter}',
-                             Engine.set_pos(self, LINE)[0],
-                             Engine.set_pos(self, LINE)[1],
-                             FONT_C, FONT_H)
+            LINE = 0
+            LINES = [f'Local frame: {self.text_local_frame_counter}',
+                     f'Global frame: {self.text_global_frame_counter}',
+                     f'FPS: {self.text_FPS}',
+                     f'Center: X: {self.sprite_player.center_x}, Y: {self.sprite_player.center_y}',
+                     f'Change: X: {self.sprite_player.change_x}, Y: {self.sprite_player.change_y}',
+                     f'Collisions: {self.text_list_collisions}']
 
-            LINE = 2
-            arcade.draw_text(f'Global frame: {self.text_global_frame_counter}',
-                             Engine.set_pos(self, LINE)[0],
-                             Engine.set_pos(self, LINE)[1],
-                             FONT_C, FONT_H)
-
-            LINE = 3
-            arcade.draw_text(f'FPS: {self.text_FPS}',
-                             Engine.set_pos(self, LINE)[0],
-                             Engine.set_pos(self, LINE)[1],
-                             FONT_C, FONT_H)
-
-            LINE = 4
-            arcade.draw_text(f'X: {self.sprite_player.center_x}, Y: {self.sprite_player.center_y}',
-                             Engine.set_pos(self, LINE)[0],
-                             Engine.set_pos(self, LINE)[1],
-                             FONT_C, FONT_H)
+            for i in range(len(LINES)):
+                LINE += 1
+                Engine.draw_text(self, LINES[i], LINE)
 
 
     def draw_UI(self):
